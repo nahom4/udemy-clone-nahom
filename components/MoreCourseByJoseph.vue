b31cc12bcf00528b60074338dba5fa6f33e169e8
Nahom <nahomamare52@gmail.com>
2024-11-12 00:49:12 +0300

    change

--- components/MoreCourseByJoseph.vue
+++ components/MoreCourseByJoseph.vue
@@ -7,9 +7,9 @@
             More Courses by <span class="text-indigo-600">Joseph Phillips</span>
         </h2>
         <div class="flex gap-2 border-b-2 border-[#D1D7DC] pb-10">
-            <CourseByJoseph src="images/lady.jpg"/>
-            <CourseByJoseph src="images/man.jpg.svg"/>
-            <CourseByJoseph src="images/team.svg"/>
+            <CourseByJoseph src="/images/lady.jpg"/>
+            <CourseByJoseph src="/images/man.jpg.svg"/>
+            <CourseByJoseph src="/images/team.svg"/>
         </div>
         <div class="text-center font-bold p-2 my-6 border-2 border-[#2D2F31] hover:bg-gray-300">Report abuse</div>
     </div>
