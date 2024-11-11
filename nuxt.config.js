7bf4a03fc7940493bfc8d23633cb3f4bbd2e4b15
Nahom <nahomamare52@gmail.com>
2024-11-11 23:36:02 +0300

    update configuration

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,44 +1,38 @@
 export default {
   router: {
-    base: '/udemy-clone-nahom/', 
+    base: '/udemy-clone-nahom/',  // Keep this if you're using a sub-path for your app
   },
 
-
-  target: 'static',
-  generate: {
-    dir: 'dist' 
-  },
+  ssr: true,  // Enable server-side rendering (SSR)
+  target: 'server',  // Set the target to 'server' for SSR deployment
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'udemy-clone',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       {
         rel: 'stylesheet',
         href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css',
-      }
-      
-    ]
-
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-     '@/assets/css/tailwind.css'
+    '@/assets/css/tailwind.css',
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -49,8 +43,9 @@ export default {
     '@nuxt/typescript-build',
     // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
-    '@nuxtjs/vuetify'
+    '@nuxtjs/vuetify',
   ],
+
   vuetify: {
     theme: {
       dark: false,  // Set your theme preferences
@@ -67,11 +62,10 @@ export default {
       },
     },
   },
+
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
+  modules: [],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  build: {},
 }
